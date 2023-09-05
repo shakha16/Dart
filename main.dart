eb6b5bb4cb2bd45d8e8da1475ef64c7e4e8158a7
Shoxrux <ganiorzikulov@gmail.com>
2023-09-05 21:11:22 +0500

    first

---
+++ main.dart
@@ -0,0 +1,60 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  //1
+  // print('Введите имя');
+  // final word = stdin.readLineSync();
+  // if (word != null && word.isNotEmpty) {
+  //   String lastLetter = word.substring(word.length - 1);
+  //   if (lastLetter == "a" || lastLetter == "A") {
+  //     print("Введите возраст");
+  //     final age = int.parse(stdin.readLineSync() ?? "0");
+  //     if (age >= 18 && age <= 30) {
+  //       print("Give me money!!!");
+  //       final money = int.parse(stdin.readLineSync() ?? "0");
+  //       if (money >= 100) {
+  //         print("You are welcome");
+  //       } else {
+  //         print("work work work!!!");
+  //       }
+  //     } else if (age > 30) {
+  //       print("old");
+  //     } else {
+  //       print("young");
+  //     }
+  //   } else {
+  //     print('Отдохни');
+  //   }
+  // } else {
+  //   print('Отдохни');
+  // }
+
+  //2
+  // print("Введите результат");
+  // final result = int.parse(stdin.readLineSync() ?? "0");
+  // if (result < 10) {
+  //   print("Ужасно");
+  // } else if (result < 30) {
+  //   print("Плохо");
+  // } else if (result < 60) {
+  //   print("Не очень хорошо");
+  // } else if (result >= 60 && result < 70) {
+  //   print("Приемлемо");
+  // } else if (result >= 70 && result < 80) {
+  //   print("Удовлетворительно");
+  // } else if (result >= 80 && result < 90) {
+  //   print("Хорошо");
+  // } else if (result >= 90 && result <= 100) {
+  //   print("Отлично");
+  // }
+
+  //3
+  // final users = ["Alex", "Bob", "Deril", "Radrigo"];
+  // print("Введите имя");
+  // final name = stdin.readLineSync();
+  // if (users.contains(name)) {
+  //   print("Пользователь найден");
+  // } else {
+  //   print("Пользователя нет в базе");
+  // }
+}
