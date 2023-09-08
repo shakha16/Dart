862183acd939bb9cf2b638aa88d29ee526f87f00
Shoxrux <ganiorzikulov@gmail.com>
2023-09-08 18:02:29 +0500

    add new homework

--- main.dart
+++ main.dart
@@ -2,59 +2,23 @@ import 'dart:io';
 
 void main(List<String> args) {
   //1
-  // print('Введите имя');
-  // final word = stdin.readLineSync();
-  // if (word != null && word.isNotEmpty) {
-  //   String lastLetter = word.substring(word.length - 1);
-  //   if (lastLetter == "a" || lastLetter == "A") {
-  //     print("Введите возраст");
-  //     final age = int.parse(stdin.readLineSync() ?? "0");
-  //     if (age >= 18 && age <= 30) {
-  //       print("Give me money!!!");
-  //       final money = int.parse(stdin.readLineSync() ?? "0");
-  //       if (money >= 100) {
-  //         print("You are welcome");
-  //       } else {
-  //         print("work work work!!!");
-  //       }
-  //     } else if (age > 30) {
-  //       print("old");
-  //     } else {
-  //       print("young");
-  //     }
-  //   } else {
-  //     print('Отдохни');
-  //   }
-  // } else {
-  //   print('Отдохни');
+  // print('Введите число');
+  // int num = int.parse(stdin.readLineSync() ?? "0");
+  // for (int i = 0; i <= num; i++) {
+  //   print(i);
   // }
 
   //2
-  // print("Введите результат");
-  // final result = int.parse(stdin.readLineSync() ?? "0");
-  // if (result < 10) {
-  //   print("Ужасно");
-  // } else if (result < 30) {
-  //   print("Плохо");
-  // } else if (result < 60) {
-  //   print("Не очень хорошо");
-  // } else if (result >= 60 && result < 70) {
-  //   print("Приемлемо");
-  // } else if (result >= 70 && result < 80) {
-  //   print("Удовлетворительно");
-  // } else if (result >= 80 && result < 90) {
-  //   print("Хорошо");
-  // } else if (result >= 90 && result <= 100) {
-  //   print("Отлично");
+  // print('Введите число');
+  // int num = int.parse(stdin.readLineSync() ?? "0");
+  // for (int i = num; i >= 0; i--) {
+  //   print(i);
   // }
 
   //3
-  // final users = ["Alex", "Bob", "Deril", "Radrigo"];
-  // print("Введите имя");
-  // final name = stdin.readLineSync();
-  // if (users.contains(name)) {
-  //   print("Пользователь найден");
-  // } else {
-  //   print("Пользователя нет в базе");
-  // }
+  // do {
+  //   print("Введите что то");
+  //   final String? dataOfStrting = stdin.readLineSync();
+  // } while (dataOfString != null && dataOfString.toString().isNotEmpty );
+  // print('Вы ввели: $num');
 }
