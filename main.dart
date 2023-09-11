0570bea17e68c39562e716cc375a585cc6ae6f54
Shoxrux <ganiorzikulov@gmail.com>
2023-09-11 17:58:58 +0500

    add homework

--- main.dart
+++ main.dart
@@ -1,24 +1,28 @@
 import 'dart:io';
 
+// void main(List<String> args) {
+//   final arr = [1, 2, 3, 4, 5, 6];
+//   print(a(arr));
+// }
+
+// a(arr) {
+//   List<int> evenNumbers = [];
+
+//   arr.forEach((number) {
+//     if (number % 2 == 0) {
+//       evenNumbers.add(number);
+//     }
+//   });
+//   return evenNumbers;
+// }
+
 void main(List<String> args) {
-  //1
-  // print('Введите число');
-  // int num = int.parse(stdin.readLineSync() ?? "0");
-  // for (int i = 0; i <= num; i++) {
-  //   print(i);
-  // }
+  print("Enter age");
+  int year = int.parse(stdin.readLineSync() ?? "0");
+  print(a(year));
+}
 
-  //2
-  // print('Введите число');
-  // int num = int.parse(stdin.readLineSync() ?? "0");
-  // for (int i = num; i >= 0; i--) {
-  //   print(i);
-  // }
-
-  //3
-  // do {
-  //   print("Введите что то");
-  //   final String? dataOfStrting = stdin.readLineSync();
-  // } while (dataOfString != null && dataOfString.toString().isNotEmpty );
-  // print('Вы ввели: $num');
+a(year) {
+  final thisYear = 2023;
+  return (thisYear - year);
 }
